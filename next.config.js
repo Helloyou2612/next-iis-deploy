8ce1799f4738608e460e1e5ac212c760d08cb450
helloyou2612 <helloyou2612@gmail.com>
2023-03-02 14:33:20 +0700

    add new code

--- next.config.js
+++ next.config.js
@@ -3,4 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const name = process.env.NEXT_PUBLIC_USERNAME || 'world';
+
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
+            &nbsp;{process.env.NEXT_PUBLIC_USERNAME} - {process.env.NEXT_PUBLIC_API_URL}
           </p>
           <div>
             <a
